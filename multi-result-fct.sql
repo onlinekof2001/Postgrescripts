d9f2787dc6b67b9779d1c068510627afb1d73bdb
onlinekof2001 <onlinekof2001@aliyun.com>
2018-11-16 16:13:07 +0800

    multi-result-fct.sql

---
+++ multi-result-fct.sql
@@ -0,0 +1,53 @@
+CREATE OR REPLACE FUNCTION list_obj_size(obj varchar) RETURNS SETOF RECORD AS 
+$$
+DECLARE
+	dblist record;
+	tablist record;
+	BEGIN 
+	IF obj = 'db'::varchar THEN
+	FOR dblist IN (SELECT d.datname as "Name",
+       pg_catalog.pg_get_userbyid(d.datdba)::text as "Owner",
+       CASE WHEN pg_catalog.has_database_privilege(d.datname, 'CONNECT')
+            THEN pg_catalog.pg_size_pretty(pg_catalog.pg_database_size(d.datname))
+            ELSE 'No Access'
+       END as "Size"
+  FROM pg_catalog.pg_database d
+  JOIN pg_catalog.pg_tablespace t on d.dattablespace = t.oid
+ WHERE d.datname not in ('template0','template1','postgres','rdsadmin')
+ ORDER BY pg_database_size(d.datname) desc) LOOP
+	RETURN next dblist;
+	END LOOP;
+	ELSIF obj = 'tab' THEN
+	FOR tablist IN (SELECT c.relname as "Name",n.nspname::text as "Owner",
+  pg_catalog.pg_size_pretty(pg_catalog.pg_table_size(c.oid)) as "Size"
+FROM pg_catalog.pg_class c
+     LEFT JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace
+WHERE c.relkind IN ('r','')
+      AND n.nspname <> 'pg_catalog'
+      AND n.nspname <> 'information_schema'
+      AND n.nspname !~ '^pg_toast'
+  AND pg_catalog.pg_table_is_visible(c.oid)
+ORDER BY pg_table_size(c.oid) desc limit 30) LOOP
+	RETURN next tablist;
+	END LOOP;
+	ELSE
+	FOR tablist IN (SELECT c.relname as "Name",n.nspname::text as "Owner",
+   pg_catalog.pg_size_pretty(pg_catalog.pg_table_size(c.oid)) as "Size"
+FROM pg_catalog.pg_class c
+     LEFT JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace
+WHERE c.relkind IN ('r','')
+      AND n.nspname <> 'pg_catalog'
+      AND n.nspname <> 'information_schema'
+      AND n.nspname !~ '^pg_toast'
+      AND c.relname LIKE 'transaction_live%'
+      AND pg_catalog.pg_table_is_visible(c.oid)
+ORDER BY pg_table_size(c.oid) desc limit 30) LOOP
+	RETURN next tablist;
+	END LOOP;
+	END IF;
+	RETURN;
+END; $$ LANGUAGE PLPGSQL;
+
+
+
+
