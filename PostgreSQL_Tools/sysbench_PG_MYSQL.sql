3659c3670342be70605d5d2554c1758523cc953b
onlinekof2001 <onlinekof2001@aliyun.com>
2019-05-13 16:44:59 +0800

    all modified scripst

---
+++ PostgreSQL_Tools/sysbench_PG_MYSQL.sql
@@ -0,0 +1,10 @@
+/* base on digoal test flow
+https://yq.aliyun.com/articles/35517
+Sysbench测试 PostgreSQL & MySQL 步骤
+知乎测试结果
+https://zhuanlan.zhihu.com/p/24992338
+sysbench download url
+https://github.com/akopytov/sysbench
+*/
+
+-- 
